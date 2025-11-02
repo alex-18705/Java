c53b45339611377a8c624d23fe4c30dbb7533dea
Nguyen Minh Tri <nguyenminhtri18072005@gmail.com>
2025-11-02 11:08:41 +0100

    Bài 3

---
+++ bai3/tinhtong.java
@@ -0,0 +1,14 @@
+package bai3;
+
+public class tinhtong {
+    public static void main(String[] args) {
+        int sum = 0;
+        // Dung for
+
+        for (int i=0; i<=10; i++){
+            sum+=i;
+        }
+        System.out.println(sum); 
+        
+    }
+}
